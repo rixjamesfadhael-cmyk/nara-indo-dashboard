1b8ebfb01a6e9998ceae0a13a4e4d0d3aed465ed
rixjamesfadhael-cmyk <rixjames.fadhael@gmail.com>
2025-12-30 23:21:44 +0700

    sync firebase web app config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
-import { signInAnonymously } from 'firebase/auth'
-import { auth } from './firebase'
-import { useEffect } from 'react'
+import { useState, useEffect } from "react";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 export default function App() {
-  useEffect(() => {
-    signInAnonymously(auth)
-  }, [])
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
 
-  return <h1>Aplikasi siap dijalankan</h1>
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      alert("Email atau password salah");
+    }
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLogin(true);
+      } else {
+        setIsLogin(false);
+      }
+    });
+  }, []);
+
+  if (!isLogin) {
+    return (
+      <div style={{ padding: 40 }}>
+        <h2>Login</h2>
+
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br /><br />
+
+        <button onClick={login}>Login</button>
+      </div>
+    );
+  }
+
+  return <h1>Aplikasi siap dijalankan</h1>;
 }
